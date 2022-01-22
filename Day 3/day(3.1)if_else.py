88fa2d2d2fe9a412921554a4684df2d97f471926
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-22 15:38:12 +0530

    day 3.1

---
+++ Day 3/day(3.1)if_else.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoster")
+height = int(input("Enter your height in cm: "))
+
+if height >= 120:
+    print("You are good to go")
+else:
+    print("Sorry, you have to grow taller before you can ride")
