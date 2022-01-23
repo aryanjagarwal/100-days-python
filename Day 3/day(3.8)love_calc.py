ebf4051b8109cfceefd6063ab39349e189c5ec27
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-23 09:52:59 +0530

    day 3.8.1 done

--- Day 3/day(3.8)love_calc.py
+++ Day 3/day(3.8)love_calc.py
@@ -19,6 +19,13 @@ e = lower_case_string.count("e")
 
 love = l + o + v + e
 
-love_score = str(true) + str(love)
+love_score = int(str(true) + str(love))
 
-print(f"Your love score is {love_score}")
+#print(f"Your love score is {love_score}")
+
+if (love_score < 10) or (love_score > 90):
+    print(f"Your love score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your love score is {love_score}, you are alright together.")
+else:
+    print(f"Your love score is {love_score}.")
