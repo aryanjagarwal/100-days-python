c9a766063177f634a1771e16c5aac1d2c78a453e
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-24 10:17:10 +0530

    day 4.4

--- Day 4/day(4.3)who_pays_Ex.py
+++ Day 4/day(4.3)who_pays_Ex.py
@@ -1,1 +1,14 @@
 import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+namesAsCSV = input("Give me everybody's name, separted by comma")
+names = namesAsCSV.split(",")
+
+num_items = len(names)
+
+random_choice = random.randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to pay the bill.")
---
+++ Day 4/day(4.4)IndexErrors.py
@@ -0,0 +1,9 @@
+states_of_america = ["Delaware", "Pennyslvania", "sghgdjd", "hdjhdjhd"]
+
+num_of_states = len(states_of_america)
+
+print(states_of_america[num_of_states - 1])
+
+ditry_dozens = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Cherries", "Peaches", "Pears", "Bell", "Celery", "Tomatoes"]
+
+# nkows
