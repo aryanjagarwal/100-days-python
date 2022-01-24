6c6a750085e57e214cabde587ebdd8bcc8722783
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-24 09:44:32 +0530

    added day 4.3

---
+++ Day 4/day(4.1)random.py
@@ -0,0 +1,7 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random()  # 0.000 to 0.99999999...
+print(random_float)
---
+++ Day 4/day(4.2)lists.py
@@ -0,0 +1,9 @@
+states_of_america = ["Delaware", "Pennyslvania", "sghgdjd", "hdjhdjhd"]
+
+print(states_of_america[1])
+print(states_of_america[-1])
+
+states_of_america[1] = "Pencilvania"
+
+states_of_america.append("Aryan")
+print(states_of_america)
