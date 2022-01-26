9a7a3ef15a7a311c2b5d36fd2f7d2eeb4e817620
Aryan Jagarwal <ajbaggar@gmail.com>
2022-01-26 14:55:59 +0530

    day 6 complete

---
+++ Day 6/day(6.3)while_loop.py
@@ -0,0 +1,27 @@
+i = 0
+while i<10:
+    print("yes " + str(i))
+    i = i + 1
+
+
+
+
+for i in range(6):
+    print("Aryan")
+
+
+
+
+j = 1
+while j<11:
+    print(f"5X{j} = {5*j}")
+    j = j + 1
+
+
+
+
+fruits = ['banana', 'watermelon', 'grapes', 'mangoes']
+i = 0
+while i<len(fruits):
+    print(fruits[i])
+    i = i + 1
