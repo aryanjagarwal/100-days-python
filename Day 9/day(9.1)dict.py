69b58d34d5ffed76bff85b8fb59e8300263bb6a0
aryanjagarwal <68804795+aryanjagarwal@users.noreply.github.com>
2022-02-16 09:11:31 +0530

    day 9.1

---
+++ Day 9/day(9.1)dict.py
@@ -0,0 +1,15 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from runnung as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing of something over and over again."
+}
+
+print(programming_dictionary["Bug"])
+print(programming_dictionary["Function"])
+print(programming_dictionary["Loop"])
+
+# Adding a new item to dict
+
+programming_dictionary["Aryan"] = "A coder"
+
+print(programming_dictionary["Aryan"])
